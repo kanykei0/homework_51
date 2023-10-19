53b3acfa5d9f068ca1cd8bf745045738c8c59eaa
Duishenalieva <liltril783@gmail.com>
2023-10-19 15:40:33 +0600

    created application for drawing lottery numbers

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
+
+  const generateRandomNumbers = () => {
+    setNumbers(()=>{
+      const newNumbers: number[] = [];
+      while (newNumbers.length < 5) {
+        const random = Math.floor(Math.random() * 32 + 5);
+        if (!newNumbers.includes(random)) {
+          newNumbers.push(random);
+        }
+      }
+      newNumbers.sort((a, b) => a - b);
+      return newNumbers;
+    });
+  };
 
   return (
-    <>
-    </>
-  )
-}
+    <div>
+      <button onClick={generateRandomNumbers}>New numbers</button>
+      <div className='numbers-block'>{numbers.join(', ')}</div>
+    </div>
+  );
+};
 
-export default App
+export default App;
